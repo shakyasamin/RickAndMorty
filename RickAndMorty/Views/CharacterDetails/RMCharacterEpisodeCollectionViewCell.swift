92a7532e74986ad1ecd55c1e4e7d896290f8c76d
saminshakya <saminshakya27@gmail.com>
2025-05-02 14:05:45 +0545

    get data for episode

--- RickAndMorty/Views/CharacterDetails/RMCharacterEpisodeCollectionViewCell.swift
+++ RickAndMorty/Views/CharacterDetails/RMCharacterEpisodeCollectionViewCell.swift
@@ -11,6 +11,8 @@ final class RMCharacterEpisodeCollectionViewCell: UICollectionViewCell{
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        contentView.backgroundColor = .systemBlue
+        contentView.layer.cornerRadius = 8
     }
     
     required init?(coder: NSCoder) {
@@ -26,6 +28,11 @@ final class RMCharacterEpisodeCollectionViewCell: UICollectionViewCell{
     }
     
     public func configure(with viewModel: RMCharacterEpisodeCollectionViewCellViewModel){
-        
+        viewModel.registerForData { data in
+            print(data.name)
+            print(data.air_date)
+            print(data.episode)
+        }
+        viewModel.fetchEpisode()
     }
 }
