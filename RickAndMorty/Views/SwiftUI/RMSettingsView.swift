e4b48ffc8266f6399a2545d5845d95af9b672919
saminshakya <saminshakya27@gmail.com>
2025-05-11 12:42:34 +0545

    created setting view swiftUI

---
+++ RickAndMorty/Views/SwiftUI/RMSettingsView.swift
@@ -0,0 +1,45 @@
+//
+//  RMSettingsView.swift
+//  RickAndMorty
+//
+//  Created by MicroBanker Nepal Pvt. Ltd. on 11/05/2025.
+//
+
+import SwiftUI
+
+struct RMSettingsView: View {
+    
+    let viewModel: RMSettingsViewViewModel
+    
+    init(viewModel: RMSettingsViewViewModel) {
+        self.viewModel = viewModel
+    }
+    
+    var body: some View {
+        List(viewModel.cellViewModels){ viewModel in
+            
+            HStack{
+                if let image = viewModel.image {
+                    Image(uiImage: image)
+                        .resizable()
+                        .renderingMode(.template)
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 20, height: 20)
+                        .foregroundColor(Color.white)
+                        .padding(8)
+                        .background(Color(viewModel.iconConatinerColor))
+                        .cornerRadius(6)
+                }
+                Text(viewModel.title)
+                    .padding(.leading, 10)
+            }
+            .padding(.bottom, 3)
+        }
+    }
+}
+
+#Preview {
+    RMSettingsView(viewModel: .init(cellViewModels: RMSettingsOption.allCases.compactMap({
+        return RMSettingsCellViewModel(type: $0)
+    })))
+}
